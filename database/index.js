ff312d408ca3131ed4c310a749383da999203666
Justin <jvcruz@ucdavis.edu>
2018-02-12 20:44:02 -0800

    browser router and db file init

---
+++ database/index.js
@@ -0,0 +1,2 @@
+const db = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App';
- 
+import { BrowserRouter as Router } from 'react-router-dom';
+
 document.addEventListener('DOMContentLoaded', function() {
   ReactDOM.render(
-    <App/>,
-    document.getElementById('app')
+  	<Router>
+      <App/>
+    <Router>
+    ,document.getElementById('app')
   );
 });
