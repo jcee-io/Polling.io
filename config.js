5a0fff86bfca58b6a8b93b1b52f5fcdf9b5172b9
Justin <jvcruz@ucdavis.edu>
2018-02-14 18:45:11 -0800

    jwt utilized for auth

---
+++ config.js
@@ -0,0 +1,1 @@
+module.exports = '932j823jh983jj2982398j239f93j023j0';
--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const Promise = require('bluebird');
 const bcrypt = Promise.promisifyAll(require('bcrypt'));
+const jwt = require('jwt-simple')
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpack = require('webpack');
 const webpackConfig = require('./webpack.config.js');
+const secretKey = require('./config');
 const app = express();
 const path = require('path');
 
@@ -13,6 +15,11 @@ const compiler = webpack(webpackConfig);
 app.use(express.static(__dirname + '/dist'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+const getToken = username => {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: username, iat: timestamp }, secretKey);
+};
 
 if(process.env.NODE_ENV !== 'production') {
   app.use(webpackDevMiddleware(compiler, {
@@ -31,7 +38,7 @@ app.post('/signup', async (req, res) => {
   const salt = await bcrypt.genSaltAsync(10);
   const password   = await bcrypt.hashAsync(req.body.password, salt);
 
-  res.json({ username, email, password });
+  res.json(getToken(username));
 });
 
 app.get('*', (req, res) => {
@@ -41,5 +48,5 @@ app.get('*', (req, res) => {
 const server = app.listen(3000, function() {
   const host = server.address().address;
   const port = server.address().port;
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log(`Example app listening at http://${host}:${port}`);
 });
