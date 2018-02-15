4ebe358ae9518496ac349986a4721f111e012654
Justin <jvcruz@ucdavis.edu>
2018-02-15 01:51:22 -0800

    database init

--- database/index.js
+++ database/index.js
@@ -1,2 +1,21 @@
-const db = require('mongoose');
-const passportLocalMongoose = require('passport-local-mongoose');
+const Promise = require('bluebird');
+const db = Promise.promisifyAll(require('mysql'));
+const connectionSQL = require('../connectionSQL');
+
+const connection = db.createConnection(connectionSQL);
+
+
+
+(async () => {
+	await connection.connect();
+	await connection.query('CREATE DATABASE IF NOT EXISTS App');
+	await connection.query('USE App');
+	await connection.query(
+		`CREATE TABLE IF NOT EXISTS Users (
+			id INTEGER PRIMARY KEY AUTO_INCREMENT,
+			username VARCHAR(255) NOT NULL,
+			password VARCHAR(255) NOT NULL,
+			email VARCHAR(255)
+		 )`
+	);
+})();
