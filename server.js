c50e41a2833fe0de5246f3828edd801dfd990922
Justin <jvcruz@ucdavis.edu>
2018-02-17 17:24:13 -0800

    process.env.PORT added

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'dist/index.html'));
 });
  
-const server = app.listen(3000, function() {
+const server = app.listen(process.env.PORT || 3000, function() {
   const host = server.address().address;
   const port = server.address().port;
   console.log(`Example app listening at http://${host}:${port}`);
