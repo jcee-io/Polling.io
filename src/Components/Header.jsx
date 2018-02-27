f294cb537145e5f723305cb699dc4bde1df02fdc
Justin <jvcruz@ucdavis.edu>
2018-02-27 03:44:47 -0800

    bootstrap navbar configured

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,25 +3,43 @@ import { Link } from 'react-router-dom';
 
 
 const NormalHeader = () => (
-	<nav>
-	  <Link to="/">Home</Link>
-  	<Link to="/signup">Sign Up</Link>
-    <Link to="/login">Login</Link>
-	</nav>
+	<ul className="navbar-nav">
+    <li className="nav-item active">
+	    <Link className="nav-link" to="/">Home</Link>
+    </li>
+    <li className="nav-item">
+  	  <Link className="nav-link" to="/signup">Sign Up</Link>
+    </li>
+    <li className="nav-item">
+      <Link className="nav-link" to="/login">Login</Link>
+    </li>
+	</ul>
 );
 
 const SecretHeader = () => (
-	<nav>
-    <Link to="/">Home</Link>
-  	<Link to="/logout">Logout</Link>
-	</nav>
+	<ul className="navbar-nav">
+    <li className="nav-item active">
+      <Link className="nav-link" to="/">Home</Link>
+    </li>
+    <li className="nav-item">
+  	  <Link className="nav-link" to="/logout">Logout</Link>
+    </li>
+	</ul>
 );
 
 export default props => (
-	<div>
-  {props.authenticated ?
-  	<SecretHeader /> :
-  	<NormalHeader />
-  }
-  </div>
+  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <a className="navbar-brand" href="#">Polling.io</a>
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+      {props.authenticated ?
+        <SecretHeader /> :
+        <NormalHeader />
+      }
+    </div>
+  </nav>
 );
+
+
