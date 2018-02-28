e08dbd5b2db02134aece0a5cfc2f140b9adbb16e
Justin <jvcruz@ucdavis.edu>
2018-02-28 01:37:55 -0800

    ViewPolls complete styling

--- src/Components/ViewPolls.jsx
+++ src/Components/ViewPolls.jsx
@@ -6,9 +6,21 @@ export default ({ polls, handler }) => (
 	<div>
 	  {polls.map(poll => 
 	  	<div>
-		  	<button name={poll.name} onClick={handler}>Delete</button>
-		  	<Link to={`/${localStorage.getItem('username')}/${urlencode(poll.name)}`}>{poll.name}</Link>
+	  		<div className="row">
+	  			<div className="col-md-8">
+	  				<Link style={{ color: 'black'}} to={`/${localStorage.getItem('username')}/${urlencode(poll.name)}`}>
+	  					<button  style={{ width: '100%'}} className="btn btn-outline-dark">
+	  					  {poll.name}
+	  					</button>
+	  				</Link>
+	  			</div>
+	  			<div style={{textAlign: 'right'}} className="col-md-4">
+	  				<button style={{ width: 100}} className="btn btn-outline-dark" name={poll.name} onClick={handler}>
+	  				  Delete
+	  				</button>
+	  			</div>
+	  		</div>
 			</div>
-) || []}
+		) || []}
 	</div>
 );
